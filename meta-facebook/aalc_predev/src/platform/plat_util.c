024ecfcac7eb3b2997c28efdd243428753681f34
Kevin Huang <KevinHuang4@quntatw.com>
2024-05-14 09:54:10 +0800

    temp save

--- meta-facebook/aalc_predev/src/platform/plat_util.c
+++ meta-facebook/aalc_predev/src/platform/plat_util.c
@@ -21,6 +21,8 @@
 #include <logging/log.h>
 #include "util_spi.h"
 #include "libutil.h"
+#include "plat_sensor_table.h"
+#include "sensor.h"
 
 #define I2C_MASTER_READ_BACK_MAX_SIZE 16 // 16 registers
 
@@ -28,6 +30,82 @@ static uint16_t temp_read_length;
 static uint16_t temp_read_data[I2C_MASTER_READ_BACK_MAX_SIZE];
 
 LOG_MODULE_REGISTER(plat_util);
+
+uint16_t read_sensor_reading(uint8_t sensor_num, uint16_t *reading_data)
+{
+	int reading = 0;
+	uint8_t status = get_sensor_reading(sensor_config, sensor_config_count, sensor_num,
+					    &reading, GET_FROM_CACHE);
+
+	if (status == SENSOR_READ_SUCCESS) {
+		sensor_val *sval = (sensor_val *)&reading;
+		float reading_result = (sval->integer * 1000) + (sval->fraction / 1000);
+		*reading_data = (uint16_t)reading_result;
+		return READ_SENSOR_SUCCESS;
+	}
+
+	LOG_ERR("modbus cmd read sensor 0x%x failed", sensor_num);
+	return READ_SENSOR_FAIL;
+}
+
+uint8_t modbus_command_read_rpu_pwr(modbus_command_mapping *cmd)
+{
+	// sum of backplane board and bridge board pwr
+	CHECK_NULL_ARG_WITH_RETURN(cmd, MODBUS_EXC_ILLEGAL_DATA_VAL);
+	uint8_t read_array[2] = { SENSOR_NUM_BB_HSC_P48V_PIN_PWR_W,
+				  SENSOR_NUM_BPB_HSC_P48V_PIN_PWR_W };
+	uint16_t rpu_pwr_val = 0, reading_data_back = 0;
+	int len = sizeof(read_array) / sizeof(read_array[0]);
+	for (int i = 0; i < len; i++) {
+		uint8_t reading_pwr_back = read_sensor_reading(read_array[i], &reading_data_back);
+		rpu_pwr_val += reading_data_back;
+		if (reading_pwr_back == READ_SENSOR_FAIL)
+			return MODBUS_EXC_SERVER_DEVICE_FAILURE;
+	}
+
+	memcpy(cmd->data, &rpu_pwr_val, sizeof(uint16_t) * cmd->cmd_size);
+	return MODBUS_EXC_NONE;
+}
+
+uint8_t modbus_command_read_aalc_total_pwr(modbus_command_mapping *cmd)
+{
+	// sum of rpu total pwr and hex total pwr
+	CHECK_NULL_ARG_WITH_RETURN(cmd, MODBUS_EXC_ILLEGAL_DATA_VAL);
+	uint8_t read_array[16] = {
+		SENSOR_NUM_BB_HSC_P48V_PIN_PWR_W,    SENSOR_NUM_BPB_HSC_P48V_PIN_PWR_W,
+		SENSOR_NUM_FB_1_HSC_P48V_PIN_PWR_W,  SENSOR_NUM_FB_2_HSC_P48V_PIN_PWR_W,
+		SENSOR_NUM_FB_3_HSC_P48V_PIN_PWR_W,  SENSOR_NUM_FB_4_HSC_P48V_PIN_PWR_W,
+		SENSOR_NUM_FB_5_HSC_P48V_PIN_PWR_W,  SENSOR_NUM_FB_6_HSC_P48V_PIN_PWR_W,
+		SENSOR_NUM_FB_7_HSC_P48V_PIN_PWR_W,  SENSOR_NUM_FB_8_HSC_P48V_PIN_PWR_W,
+		SENSOR_NUM_FB_9_HSC_P48V_PIN_PWR_W,  SENSOR_NUM_FB_10_HSC_P48V_PIN_PWR_W,
+		SENSOR_NUM_FB_11_HSC_P48V_PIN_PWR_W, SENSOR_NUM_FB_12_HSC_P48V_PIN_PWR_W,
+		SENSOR_NUM_FB_13_HSC_P48V_PIN_PWR_W, SENSOR_NUM_FB_14_HSC_P48V_PIN_PWR_W
+	};
+
+	uint16_t rpu_pwr_val = 0, reading_data_back = 0;
+	int len = sizeof(read_array) / sizeof(read_array[0]);
+	for (int i = 0; i < len; i++) {
+		uint8_t reading_pwr_back = read_sensor_reading(read_array[i], &reading_data_back);
+		rpu_pwr_val += reading_data_back;
+		if (reading_pwr_back == READ_SENSOR_FAIL)
+			return MODBUS_EXC_SERVER_DEVICE_FAILURE;
+	}
+
+	memcpy(cmd->data, &rpu_pwr_val, sizeof(uint16_t) * cmd->cmd_size);
+	return MODBUS_EXC_NONE;
+}
+
+uint8_t modbus_command_read_hex_fan_pwr_tach_pct(modbus_command_mapping *cmd)
+{
+	// TO DO
+	return true;
+}
+
+uint8_t modbus_command_read_hex_pwr(modbus_command_mapping *cmd)
+{
+	// TO DO
+	return true;
+}
 
 uint8_t modbus_command_i2c_master_write_read(modbus_command_mapping *cmd)
 {
--- meta-facebook/aalc_predev/src/platform/plat_util.h
+++ meta-facebook/aalc_predev/src/platform/plat_util.h
@@ -13,6 +13,19 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#ifndef PLAT_UTIL_H
+#define PLAT_UTIL_H
+
 uint8_t modbus_command_i2c_master_write_read(modbus_command_mapping *cmd);
 uint8_t modbus_command_i2c_master_write_read_response(modbus_command_mapping *cmd);
+uint8_t modbus_command_read_rpu_pwr(modbus_command_mapping *cmd);
+uint8_t modbus_command_read_aalc_total_pwr(modbus_command_mapping *cmd);
 void regs_reverse(uint16_t reg_len, uint16_t *data);
+
+enum READNG_SENSOR_RESAULT
+{
+    READ_SENSOR_SUCCESS,
+    READ_SENSOR_FAIL,
+};
+
+#endif
